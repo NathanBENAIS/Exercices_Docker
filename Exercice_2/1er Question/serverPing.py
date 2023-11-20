e60313bbb50d3b83e6e020a09f70cb72581b40c0
NathanBENAIS <96385008+NathanBENAIS@users.noreply.github.com>
2023-11-20 11:59:04 +0100

    Exercice2 1er Question

---
+++ Exercice_2/1er Question/serverPing.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import requests
+import time
+from threading import Thread
+
+app = Flask(__name__)
+server_ping_url = "http://localhost:5372"
+server_3_url = "http://localhost:6789"
+
+def send_ping():
+    while True:
+        time.sleep(0.5)
+        try:
+            response = requests.get(server_ping_url + "/ping")
+            print("Server Ping received ping from Server Pong:", response.text)
+        except requests.exceptions.RequestException as e:
+            print("Error sending ping:", e)
+
+@app.route('/pong')
+def pong():
+    return "pong"
+
+@app.route('/send_address')
+def send_address():
+    try:
+        response = requests.post(server_3_url + "/receive_address", json={"address": server_ping_url})
+        print("Server Ping sent its address to Server 3")
+        return "Address sent to Server 3"
+    except requests.exceptions.RequestException as e:
+        print("Error sending address to Server 3:", e)
+        return "Error sending address"
+
+if __name__ == '__main__':
+    ping_thread = Thread(target=send_ping)
+    ping_thread.start()
+    
+    # Envoyer l'adresse au serveur 3 au démarrage
+    send_address()
+    
+    app.run(port=4567)
---
+++ Exercice_2/1er Question/serverPong.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import requests
+import time
+from threading import Thread
+
+app = Flask(__name__)
+server_pong_url = "http://localhost:4567"
+server_3_url = "http://localhost:6789"
+
+def send_pong():
+    while True:
+        time.sleep(0.5)
+        try:
+            response = requests.get(server_pong_url + "/pong")
+            print("Server Pong received ping from Server Ping:", response.text)
+        except requests.exceptions.RequestException as e:
+            print("Error sending pong:", e)
+
+@app.route('/ping')
+def ping():
+    return "ping"
+
+@app.route('/send_address')
+def send_address():
+    try:
+        response = requests.post(server_3_url + "/receive_address", json={"address": server_pong_url})
+        print("Server Pong sent its address to Server 3")
+        return "Address sent to Server 3"
+    except requests.exceptions.RequestException as e:
+        print("Error sending address to Server 3:", e)
+        return "Error sending address"
+
+if __name__ == '__main__':
+    pong_thread = Thread(target=send_pong)
+    pong_thread.start()
+    
+    # Envoyer l'adresse au serveur 3 au démarrage
+    send_address()
+    
+    app.run(port=5372)
